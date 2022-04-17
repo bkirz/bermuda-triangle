149591766f0072b2a58a9b38f04bcfc2342ca019
Ben Kirzhner <ben.kirzhner@gmail.com>
2022-04-17 11:29:32 -0700

    Refactor for clarity

--- app.py
+++ app.py
@@ -36,9 +36,16 @@ def upload_file():
     if file.filename == '':
         return redirect(request.url)
 
-    ssc = simfile.loads(file.stream.read().decode("utf-8"))
+    # It's perhaps bold to assume all ssc files are utf-8 encoded, but I'm
+    # not sure of a better alternative.
+    input_file_contents = file.stream.read().decode("utf-8")
+    ssc = simfile.loads(input_file_contents)
 
     fixedscroll(ssc) # mutates the ssc in-place to normalize its scroll rate across bpm changes
     filename = file.filename.replace('.ssc', '-normalized.ssc')
 
-    return send_file(io.BytesIO(str(ssc).encode("utf-8")), as_attachment=True, download_name=filename)
+    # Again, assume utf-8. This should be safe since the input was assumed to be utf-8
+    # and AFAICT simfile preserves encoding.
+    output_io = io.BytesIO(str(ssc).encode("utf-8"))
+
+    return send_file(output_io, as_attachment=True, download_name=filename)
